7171e1f71f51c04f609c0195659b0f62d47d35c9
Reed Santos <reedsantos@icloud.com>
2020-11-25 07:54:45 -0500

    employee cards now show in output

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const cheerio = require("cheerio");
-const open = require("open");
 const fs = require("fs");
 const inquire = require("inquirer");
 const employee = require("./js/employee");
-
+const manager = require("./js/manager");
+const intern = require("./js/intern");
+const engineer = require("./js/engineer");
+const cheerio = require("cheerio");
+const open = require("open");
 
 let empID = 0;
 
@@ -71,14 +73,15 @@ const questionList = [
         }
     },
 ]
+
 function collectEmployee(retFunction) {
 
 
     inquire
         .prompt(questionList)
         .then(function (response) {
-
-            empID += 1;
+            
+            empID+=1;
 
             let emp;
 
@@ -100,18 +103,18 @@ function collectEmployee(retFunction) {
 
 function addEmployee() {
     inquire
-        .prompt(loopQuestion)
+        .prompt(loopQuestion) 
         .then(function (response) {
             if (response.empAdd)
                 collectEmployee(addEmployee);
             else
                 writeHTMLFile();
-        });
-};
+    });
+}
 
 function writeHTMLFile() {
-    let profileHTML = fs.readFileSync("./template.html", "utf8", function (err) {
-        if (err)
+    let profileHTML = fs.readFileSync("./template.html", "utf8", function(err) {
+        if (err) 
             console.log(err);
     });
 
@@ -134,16 +137,73 @@ function writeHTMLFile() {
         $("#TeamCards").append(output);
     });
 
-    fs.writeFile("./team_output.html", $.html(), function (err) {
+    fs.writeFile("./team_output.html", $.html(), function(err){
         if (err)
             console.log(err);
-
+        
         openTeamFile("./team_output.html")
     });
 }
 
-async function openTeamFile(fileName) {
+async function openTeamFile (fileName) {
 
-    await open(fileName, { wait: true });
+    await open(fileName, {wait:true});
     console.log("Opening file");
-};
+}
+
+function generateManagerCard(name, id, email, room) {
+    return `<div class="card m-2" style="width: 18rem;">
+    <div class="card-body bg-primary text-white">
+      <h5 class="card-title">${name}</h5>
+      <h5 class="card-title">Manager</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Id: ${id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${email}" class="card-link">${email}</a></li>
+      <li class="list-group-item">RoomNumber: ${room}</li>
+    </ul>
+    </div>`
+}
+
+function generateInternCard(name, id, email, school) {
+    return `<div class="card m-2" style="width: 18rem;">
+    <div class="card-body bg-primary text-white">
+      <h5 class="card-title">${name}</h5>
+      <h5 class="card-title">Intern</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Id: ${id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${email}" class="card-link">${email}</a></li>
+      <li class="list-group-item">School: ${school}</li>
+    </ul>
+    </div>`
+}
+
+function generateEmployeeCard(name, id, email) {
+    return `<div class="card m-2" style="width: 18rem;">
+    <div class="card-body bg-primary text-white">
+      <h5 class="card-title">${name}</h5>
+      <h5 class="card-title">Employee</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Id: ${id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${email}" class="card-link">${email}</a></li>
+    </ul>
+    </div>`
+}
+
+function generateEngineerCard(name, id, email, github) {
+    return `<div class="card m-2" style="width: 18rem;">
+    <div class="card-body bg-primary text-white">
+      <h5 class="card-title">${name}</h5>
+      <h5 class="card-title">Engineer</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Id: ${id}</li>
+      <li class="list-group-item">Email: <a href="mailto:${email}" class="card-link">${email}</a></li>
+      <li class="list-group-item">Github: <a href="https://github.com/${github}" class="card-link">${github}</a></li>
+    </ul>
+    </div>`
+}
+
+addEmployee();
