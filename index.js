b972faa9490683de3a0df91dfc5f81f8574c6acd
Reed Santos <reedsantos@icloud.com>
2020-11-25 07:50:47 -0500

    inquirer questions change based on role

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cheerio = require("cheerio");
 const open = require("open");
 const fs = require("fs");
 const inquire = require("inquirer");
-const employee = require("./assets/js/employee");
+const employee = require("./js/employee");
 
 
 let empID = 0;
@@ -70,4 +70,41 @@ const questionList = [
             return answers.empRole === "Engineer"
         }
     },
-];
+]
+function collectEmployee(retFunction) {
+
+
+    inquire
+        .prompt(questionList)
+        .then(function (response) {
+
+            empID += 1;
+
+            let emp;
+
+            if (response.empRole === "Manager") {
+                emp = new manager(response.empName, empID, response.empEmail, response.empOffice);
+            } else if (response.empRole === "Engineer") {
+                emp = new engineer(response.empName, empID, response.empEmail, response.empGitHub);
+            } else if (response.empRole === "Employee") {
+                emp = new employee(response.empName, empID, response.empEmail);
+            } else if (response.empRole === "Intern") {
+                emp = new intern(response.empName, empID, response.empEmail, response.empSchool);
+            }
+
+            employees.push(emp);
+
+            retFunction();
+        });
+}
+
+function addEmployee() {
+    inquire
+        .prompt(loopQuestion)
+        .then(function (response) {
+            if (response.empAdd)
+                collectEmployee(addEmployee);
+            else
+                newFunction();
+        });
+};
